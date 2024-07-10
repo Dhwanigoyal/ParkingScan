8e580f19e6df3d732804a67b6a70a03f6d3c16df
Dhwanigoyal <139227134+Dhwanigoyal@users.noreply.github.com>
2024-07-10 12:04:32 +0530

    Add files via upload

---
+++ ParkingScan.py
@@ -0,0 +1,31 @@
+import customtkinter as ctk
+from LoginPage import loginScreen
+from MainPage import Dashboard
+
+
+def main_window(app):
+    for widget in app.winfo_children():
+        widget.destroy()
+
+    screen_width = app.winfo_screenwidth()
+    screen_height = app.winfo_screenheight()
+    app.geometry(f"{screen_width}x{screen_height}")
+    app.title("Login")
+
+    frame_width = 500
+    frame_height = 600
+    frame_x = screen_width // 2
+    frame_y = (screen_height - 50) // 2
+
+    Dashboard(app)
+    # loginScreen(app, frame_width, frame_height, frame_x, frame_y)
+
+
+def main():
+    app = ctk.CTk()
+    main_window(app)
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ParkingScan
+# Parking Scan
+Parking Scan is a Python application using Tkinter, featuring a login screen and multiple pages for managing and visualizing data from a parking lot detection system.
+
+### The entry point for the application is `ParkingScan.py`.
+
